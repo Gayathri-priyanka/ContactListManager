29adb24aeb17c3c30ede913ff7d0ed588d54ef43
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2025-01-16 14:57:00 -0500

    Created Contact Schema and model

--- api/index.js
+++ api/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+mongoose.connect(process.env.MONGO)
+
+.then(()=>{
+    console.log("MongoDB is Connected")
+})
+.catch((err)=>{
+    console.log(err);
+})
+
 
 const app=express();
+app.use(express.json());
+
 
 app.listen(3000,()=>{
     console.log("Server is running on 3000");
