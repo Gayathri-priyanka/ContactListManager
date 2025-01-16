c5c90c859a273f96d7cddb0a52c6907f3bcaa59e
Gayathri-priyanka <gayathripriyankanamburu1@gmail.com>
2025-01-16 15:02:43 -0500

    Implemented POST /contacts route to handle adding new contacts

--- api/index.js
+++ api/index.js
@@ -16,8 +16,25 @@ mongoose.connect(process.env.MONGO)
 
 const app=express();
 app.use(express.json());
-
+app.use(cors({
+    origin: 'http://localhost:5173', // Allow requests only from this domain
+    methods: ['GET', 'POST', 'DELETE'],
+  }));
 
 app.listen(3000,()=>{
     console.log("Server is running on 3000");
 });
+
+app.post('/contacts', async (req, res) => {
+    try {
+      const { name, email } = req.body;
+      if (!name || !email) {
+        return res.status(400).send("Name and Email are required.");
+      }
+      const newContact = new Contact({ name, email });
+      await newContact.save();
+      res.status(201).send(newContact);
+    } catch (error) {
+      res.status(500).send(error.message);
+    }
+  });
